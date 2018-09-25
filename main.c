cdcac8cbd19946b046cd884e74db0bf2c087c927
tinhbuiv <tinhbvth1809016@fpt.edu.vn>
2018-09-25 10:37:48 +0700

    btbuoi1

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    int a;
+    int b;
+    printf("nhap vao so thu 1 :");
+    scanf("%d",&a);
+    printf("nhap vao so thu 2 :");
+    scanf("%d",&b);
+    printf("ket qua la a + b = : %d + %d = %d",a, b,a+b);
+    return 0;
+
+}
